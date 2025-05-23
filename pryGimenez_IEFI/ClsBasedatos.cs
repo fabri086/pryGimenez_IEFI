6b89eb2cab38157e33dc3fae3197b7cbd28b3fd4
Fabri <Fabri@DESKTOP-JT924IA>
2025-05-22 23:47:44 -0300

    Actualizado cambios

---
+++ pryGimenez_IEFI/ClsBasedatos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb; // Agregamos  System.Data.OleDb y System.Data
+using System.Data;
+
+namespace pryGimenez_IEFI
+{
+    internal class ClsBasedatos
+    {
+        OleDbConnection coneccionBD;
+        OleDbCommand comandoBD;
+        OleDbDataReader corriendoBD;
+        OleDbDataAdapter adaptadorBD;
+
+        DataSet obj; // esto es dato de memoria
+
+        string rutabasedatos;
+        public string estado;
+
+        public void Usuario()
+        {
+            try
+            {
+                //rutabasedatos=
+            }
+            catch(Exception error)
+            {
+
+            }
+
+        }
+
+    }
+
+}
